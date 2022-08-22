92a4f03fdff8c95cf3f0a98091843f5ace868fe3
DIJANGO ALVES RODOLFO <dijango_ilha@hotmail.com>
2022-08-22 14:38:37 -0300

    feat: add generate file

--- ordenarArquivos/src/ordenararquivos/OrdenarArquivos.java
+++ ordenarArquivos/src/ordenararquivos/OrdenarArquivos.java
@@ -4,14 +4,15 @@
  */
 package ordenararquivos;
 
-import java.io.BufferedReader;
-
-import java.io.File;
-import java.io.FileReader;
-import java.nio.file.Files;
-import java.util.Scanner;
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import static java.lang.Integer.parseInt;
+import java.util.Arrays;
+import java.util.Scanner;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,70 +26,69 @@ public class OrdenarArquivos {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
-//        int N[] = new int[]{100, 1000, 10000, 50000, 100000};
-        int N[] = new int[]{100};
+        int N[] = new int[]{100, 1000, 10000, 50000, 100000};
 
-        for (int i = 0; i < 5; i++) {
-            dirPasta(N[i]);
+        for (int i = 0; i < N.length; i++) {
+            String dirName = String.valueOf(N[i]);
+            int[] newArray = new int[N[i]];
+
+            Files.list(new File(dirName).toPath())
+                    .limit(10)
+                    .forEach(path -> {
+                        try {
+                            readFile(path.toString(), newArray);
+                            String newFile = path.toString().substring(0, path.toString().length() - 4) + "_ord.txt";
+                            System.out.println("newFile to save => " + newFile);
+                            Arrays.sort(newArray);
+
+                            saveOrderFile(newFile, newArray);
+                        } catch (IOException ex) {
+                            Logger.getLogger(OrdenarArquivos.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                    });
         }
-
     }
 
-    private static void dirPasta(int x) throws IOException {
-        String dirName = String.valueOf(x);
-
-        Files.list(
-                new File(dirName).toPath())
-                .limit(10)
-                .forEach((path) -> {
-                    System.out.println("########################");
-
-                    System.out.println("Aqui " + path.toString());
-                    String[] fileName = path.toString().split("\\\\");
-                    System.out.println("Arquivo " + fileName[1]);
-                    BufferedReader buffRead;
-                    try {
-                        buffRead = new BufferedReader(new FileReader(path.toString()));
-                    } catch (FileNotFoundException ex) {
-                        Logger.getLogger(OrdenarArquivos.class.getName()).log(Level.SEVERE, null, ex);
-                    }
-
-                    int N = Integer.parseInt(buffRead.readLine());
-                    int vet[] = new int[N];
-
-                    System.out.println("Novo arq => " + path.toString().substring(0, path.toString().length() - 4) + "_ord.txt");
-                    for (int i = 0; i < N; i++) {
-                        vet[i] = Integer.parseInt(buffRead.readLine());
-                    }
-                    buffRead.close();
-
-                    System.out.println("Valores lidos do arquivo");
-                    for (int i = 0; i < N; i++) {
-                        System.out.println(vet[i]);
-                    }
-//
-//                    try {
-//                        lerArquivo(path.toString());
-//                    } catch (IOException ex) {
-//                        Logger.getLogger(OrdenarArquivos.class.getName()).log(Level.SEVERE, null, ex);
-//                    }
-                }
-                );
-        System.out.println("novo arsr " + myNum);
-    }
-
-    private static void lerArquivo(String arq) throws IOException {
+    private static void readFile(String file, int[] arr) throws IOException {
         try {
-            File myObj = new File(arq);
-            Scanner myReader = new Scanner(myObj);
-            while (myReader.hasNextLine()) {
-                String data = myReader.nextLine();
-                System.out.println(data);
+            System.out.println("Chegou um arquivo => " + file);
+            File myObj = new File(file);
+            int count = 0;
+
+            try ( Scanner myReader = new Scanner(myObj)) {
+                while (myReader.hasNextLine()) {
+                    String data = myReader.nextLine();
+                    arr[count] = parseInt(data);
+                    count++;
+                }
             }
-            myReader.close();
         } catch (FileNotFoundException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
     }
+
+    private static void saveOrderFile(String path, int[] arr) throws IOException {
+        try {
+            File newFile = new File(path);
+            newFile.delete();
+            if (newFile.createNewFile()) {
+            newFile.createNewFile();
+            System.out.println("File created: " + newFile.getName());
+            } else {
+                System.out.println("File already exists.");
+            }
+
+            BufferedWriter buffWrite = new BufferedWriter(new FileWriter(path, true));
+            for (int i = 0; i < arr.length; i++) {
+                buffWrite.append(arr[i] + "\n");
+            }
+            buffWrite.close();
+
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
 }
