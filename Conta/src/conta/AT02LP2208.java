13a63cbb8d7fb488729d01c793db6177725acbde
Dijango Alves Rodolfo <dijango_ilha@hotmail.com>
2022-08-29 17:38:38 -0300

    feat: add create and write file

--- Conta/src/conta/AT02LP2208.java
+++ Conta/src/conta/AT02LP2208.java
@@ -8,6 +8,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -35,44 +36,71 @@ public class AT02LP2208 {
             File csvFile = new File(file);
             FileReader fr = new FileReader(csvFile);
             BufferedReader br = new BufferedReader(fr);
-            int count = 0;
+            ArrayList<Cadastro> registro = new ArrayList<Cadastro>();
+            String header = "";
 
             try ( Scanner myReader = new Scanner(br)) {
                 while (myReader.hasNextLine()) {
-                    String data = count != 0 ? myReader.nextLine() : "";
+                    String data = myReader.nextLine();
                     String splitData[] = new String[23];
                     splitData = data.split(",");
 
-                    if (!"".equals(data)) {
-                        Cadastro cadastro = new Cadastro(splitData[0],
-                                splitData[1],
-                                splitData[2],
-                                splitData[3],
-                                splitData[4],
-                                splitData[5],
-                                splitData[6],
-                                splitData[7],
-                                splitData[8],
-                                splitData[9],
-                                splitData[10],
-                                splitData[11],
-                                splitData[12],
-                                splitData[13],
-                                splitData[14],
-                                splitData[15],
-                                splitData[16],
-                                splitData[17],
-                                splitData[18],
-                                splitData[19],
-                                splitData[20],
-                                splitData[21],
-                                splitData[22]);
+                    if (splitData[0].equals("nome")) {
+                        header = data;
                     }
-                    System.out.println("data => " + data);
-                    count++;
+
+                    if (!splitData[0].equals("nome")) {
+                        Cadastro cadastro = new Cadastro(splitData[0],
+                                splitData[1], splitData[2], splitData[3],
+                                splitData[4], splitData[5], splitData[6],
+                                splitData[7], splitData[8], splitData[9],
+                                splitData[10], splitData[11], splitData[12],
+                                splitData[13], splitData[14], splitData[15],
+                                splitData[16], splitData[17], splitData[18],
+                                splitData[19], splitData[20], splitData[21],
+                                splitData[22]);
+                        registro.add(cadastro);
+                    }
                 }
+
+                createFile("dados_ordenados.txt");
+                writeFile("dados_ordenados.txt", header, registro);
             }
         } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    static boolean createFile(String file) {
+        try {
+            File newFile = new File(file);
+            if (newFile.createNewFile()) {
+                System.out.println("File created: " + newFile.getName());
+                return true;
+            } else {
+                System.out.println("File already exists.");
+                return false;
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    static void writeFile(String file, String header, ArrayList registros) {
+        try {
+            FileWriter myFile = new FileWriter(file);
+
+            myFile.write(header + "\n");
+
+            for (int i = 0; i < registros.size(); i++) {
+                myFile.write(registros.get(i).toString());
+            }
+            myFile.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
             System.out.println("An error occurred.");
             e.printStackTrace();
         }
--- Conta/src/conta/Cadastro.java
+++ Conta/src/conta/Cadastro.java
@@ -60,23 +60,44 @@ public class Cadastro {
         this.cor = cor;
     }
 
+    public String toString() {
+        String cadastro = cadastro = this.nome + ", "
+                + this.idade + ", "
+                + this.cpf + ", "
+                + this.rg + ", "
+                + this.data_nasc + ", "
+                + this.sexo + ", "
+                + this.signo + ", "
+                + this.mae + ", "
+                + this.pai + ", "
+                + this.email + ", "
+                + this.senha + ", "
+                + this.cep + ", "
+                + this.endereco + ", "
+                + this.numero + ", "
+                + this.bairro + ", "
+                + this.cidade + ", "
+                + this.estado + ", "
+                + this.telefone_fixo + ", "
+                + this.celular + ", "
+                + this.altura + ", "
+                + this.peso + ", "
+                + this.tipo_sanguineo + ", "
+                + this.cor + "\n";
 
+        return cadastro;
+    }
+
+//    public Cadastro sort(Cadastro a, Cadastro b) {
+//        if (a.nome < b.nome) {
+//            return a;
+//        } else {
+//        }
+//
+//        return b;
+//    }
     Cadastro() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
-    Cadastro(String[] vet) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-//    public Conta (int numero, String nome, int idade) {
-//        this.numero = numero;
-//        this.nome = nome;
-//        this.idade = idade;
-//    }
-    public String toString() {
-        String cadastro = this.nome + " " + this.numero;
-
-        return cadastro;
-    }
 }
